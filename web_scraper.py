55d10f39308c46b0fa8a45bbb3bffdb72463a85e
Bartłomiej Jamiołkowski <61807667+MindBreaker20@users.noreply.github.com>
2024-06-05 12:10:18 +0200

    version 5.1.0

--- web_scraper.py
+++ web_scraper.py
@@ -17,79 +17,101 @@ def initialize_driver() -> tuple:
 
 def scrape_krs_data(start_num: int, end_num: int) -> pd.DataFrame:
     driver_organizations, driver_representatives, driver_connections = initialize_driver()
-    krs_dataset = pd.DataFrame(columns=['KRS', 'OrganizationName', 'LegalForm', 'Representatives', 'RepresentativesID', 'ConnectedOrganizations'])
+    krs_dataset = pd.DataFrame(columns=['KRS', 'OrganizationName', 'LegalForm', 'Representatives', 'RepresentativesID',
+                                        'ConnectedOrganizations'])
 
     for krs_num in range(start_num, end_num):
         print(f'KRS: {krs_num}')
-
         url = 'https://rejestr.io/krs/' + str(krs_num)
         driver_organizations.get(url)
 
         if driver_organizations.title != 'Nie znaleziono strony | Rejestr.io':
+            try:
+                organization_name = driver_organizations.find_element(By.XPATH, '//div[@class="h1_wrapper"]/h1/a').text
+            except NoSuchElementException:
+                organization_name = np.nan
 
-            organization_name = driver_organizations.find_element(By.XPATH, '//div[@class="h1_wrapper"]/h1/a').text
-            legal_form = driver_organizations.find_element(By.CSS_SELECTOR, "div[data-name='forma_prawna'] .val.text-normalize span").text
-            link_to_representatives = driver_organizations.find_element(By.LINK_TEXT, 'Powiązania').get_attribute("href")
-            driver_representatives.get(link_to_representatives)
-            representatives = driver_representatives.find_elements(By.CSS_SELECTOR, "div[class='media object person']")
-            representatives_id = driver_representatives.find_elements_by_css_selector('li[data-global-id]')
-            list_of_representatives_id = []
-            count = 0
+            try:
+                legal_form = driver_organizations.find_element(By.CSS_SELECTOR,
+                                                               "div[data-name='forma_prawna'] .val.text-normalize span").text
+            except NoSuchElementException:
+                legal_form = np.nan
 
-            for id in representatives_id:
-                rep_id = id.get_attribute("data-global-id")
-                rep_id = ''.join(filter(str.isdigit, rep_id))
-                list_of_representatives_id.append(rep_id)
+            try:
+                link_to_representatives = driver_organizations.find_element(By.LINK_TEXT, 'Powiązania').get_attribute(
+                    "href")
+            except NoSuchElementException:
+                link_to_representatives = None
 
-            for representative in representatives:
-                krs_dataset_row = dict.fromkeys(['KRS', 'OrganizationName', 'LegalForm', 'Representatives', 'RepresentativesID', 'ConnectedOrganizations'])
-                krs_dataset_row['KRS'] = krs_num
-                krs_dataset_row['LegalForm'] = legal_form
-                krs_dataset_row['OrganizationName'] = '"' + organization_name + '"'
-                krs_dataset_row['Representatives'] = representative.text
-                krs_dataset_row['RepresentativesID'] = list_of_representatives_id[count]
+            if link_to_representatives:
+                driver_representatives.get(link_to_representatives)
+                representatives = driver_representatives.find_elements(By.CSS_SELECTOR,
+                                                                       "div[class='media object person']")
+                representatives_id = driver_representatives.find_elements(By.CSS_SELECTOR, 'li[data-global-id]')
+                list_of_representatives_id = []
+                count = 0
 
-                link_to_connections = None
-                try:
-                    link_to_connections = driver_representatives.find_element(By.LINK_TEXT, representative.text).get_attribute("href")
-                except NoSuchElementException:
-                    pass
+                for id in representatives_id:
+                    rep_id = id.get_attribute("data-global-id")
+                    rep_id = ''.join(filter(str.isdigit, rep_id))
+                    list_of_representatives_id.append(rep_id)
 
-                if link_to_connections:
-                    list_of_connections = []
-                    list_of_connections_krs = []
-                    driver_connections.get(link_to_connections)
-                    connections = driver_connections.find_elements(By.CSS_SELECTOR, "div[class='media object krs org']")
-                    connections_krs = driver_connections.find_elements_by_css_selector('li[data-global-id]')
+                for representative in representatives:
+                    krs_dataset_row = dict.fromkeys(
+                        ['KRS', 'OrganizationName', 'LegalForm', 'Representatives', 'RepresentativesID',
+                         'ConnectedOrganizations'])
+                    krs_dataset_row['KRS'] = krs_num
+                    krs_dataset_row['LegalForm'] = legal_form
+                    krs_dataset_row['OrganizationName'] = '"' + organization_name + '"'
+                    krs_dataset_row['Representatives'] = representative.text
+                    krs_dataset_row['RepresentativesID'] = list_of_representatives_id[count]
 
-                    for connection_krs in connections_krs:
-                        org_num = connection_krs.get_attribute("data-global-id")
-                        org_num = ''.join(filter(str.isdigit, org_num))
-                        if int(org_num) != krs_num:
-                            list_of_connections_krs.append(org_num)
+                    link_to_connections = None
+                    try:
+                        link_to_connections = driver_representatives.find_element(By.LINK_TEXT,
+                                                                                  representative.text).get_attribute(
+                            "href")
+                    except NoSuchElementException:
+                        pass
 
-                    for connection in connections:
-                        if "Obecnie: " in connection.text:
-                            actual_connection_name = connection.text.split("Obecnie: ")[1].strip()
-                            if len(connections) == 1 and actual_connection_name == organization_name:
-                                krs_dataset_row['ConnectedOrganizations'] = np.nan
-                            elif len(connections) > 1 and actual_connection_name == organization_name:
-                                continue
-                            elif len(connections) > 1 and actual_connection_name != organization_name:
-                                list_of_connections.append(actual_connection_name)
-                        else:
-                            if len(connections) == 1 and connection.text == organization_name:
-                                krs_dataset_row['ConnectedOrganizations'] = np.nan
-                            elif len(connections) > 1 and connection.text == organization_name:
-                                continue
-                            elif len(connections) > 1 and connection.text != organization_name:
-                                list_of_connections.append(connection.text)
-                    krs_dataset_row['ConnectedOrganizations'] = str(dict(zip(list_of_connections, list_of_connections_krs))).replace('{', '').replace('}', '') #', '.join(map(lambda x: f"'{x}'", list_of_connections))
-                else:
-                    krs_dataset_row['ConnectedOrganizations'] = np.nan
+                    if link_to_connections:
+                        list_of_connections = []
+                        list_of_connections_krs = []
+                        driver_connections.get(link_to_connections)
+                        connections = driver_connections.find_elements(By.CSS_SELECTOR,
+                                                                       "div[class='media object krs org']")
+                        connections_krs = driver_connections.find_elements(By.CSS_SELECTOR, 'li[data-global-id]')
 
-                krs_dataset = pd.concat([krs_dataset, pd.DataFrame([krs_dataset_row])], ignore_index=True)
-                count += 1
+                        for connection_krs in connections_krs:
+                            org_num = connection_krs.get_attribute("data-global-id")
+                            org_num = ''.join(filter(str.isdigit, org_num))
+                            if int(org_num) != krs_num:
+                                list_of_connections_krs.append(org_num)
+
+                        for connection in connections:
+                            if "Obecnie: " in connection.text:
+                                actual_connection_name = connection.text.split("Obecnie: ")[1].strip()
+                                if len(connections) == 1 and actual_connection_name == organization_name:
+                                    krs_dataset_row['ConnectedOrganizations'] = np.nan
+                                elif len(connections) > 1 and actual_connection_name == organization_name:
+                                    continue
+                                elif len(connections) > 1 and actual_connection_name != organization_name:
+                                    list_of_connections.append(actual_connection_name)
+                            else:
+                                if len(connections) == 1 and connection.text == organization_name:
+                                    krs_dataset_row['ConnectedOrganizations'] = np.nan
+                                elif len(connections) > 1 and connection.text == organization_name:
+                                    continue
+                                elif len(connections) > 1 and connection.text != organization_name:
+                                    list_of_connections.append(connection.text)
+                        krs_dataset_row['ConnectedOrganizations'] = str(
+                            dict(zip(list_of_connections, list_of_connections_krs))).replace('{', '').replace('}',
+                                                                                                              '')  # ', '.join(map(lambda x: f"'{x}'", list_of_connections))
+                    else:
+                        krs_dataset_row['ConnectedOrganizations'] = np.nan
+
+                    krs_dataset = pd.concat([krs_dataset, pd.DataFrame([krs_dataset_row])], ignore_index=True)
+                    count += 1
 
     driver_organizations.quit()
     driver_representatives.quit()
@@ -99,9 +121,7 @@ def scrape_krs_data(start_num: int, end_num: int) -> pd.DataFrame:
 
 if __name__ == '__main__':
     start_num = 20000
-    end_num = 20004
+    end_num = 20100
     krs_dataset = scrape_krs_data(start_num, end_num)
     krs_dataset.to_csv(f'data/krs_data_{start_num}_{end_num}.csv', encoding='utf-8-sig', index=False, sep=',',
                        quotechar='"', quoting=csv.QUOTE_ALL)
-
-
